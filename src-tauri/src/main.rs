0184840936d948f756eb6dca1e01a309064a8fd3
SimonGit <15715335111@163.com>
2026-05-27 07:45:44 +0800

    Edit yml for github

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ mod stream;
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![stream::stream_fetch])
-    .plugin(tauri_plugin_window_state::Builder::default().build())
+    // .plugin(tauri_plugin_window_state::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
